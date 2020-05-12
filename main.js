da696064dab2b55e06f8dfdd804a67ee39ccb32f
jikimfx <jikimfx@gmail.com>
2020-05-12 08:29:50 +0700

    Prettify

--- main.js
+++ main.js
@@ -46,28 +46,21 @@ let btn = document.getElementById("convertButton");
 
 convertButton.addEventListener("click", () => {
     let amt = amountConvert.value;
-    amt ? "" : alert("U must enter a number to convert");
-    amt = parseInt(amt);
+    amt ? "" : alert("Please enter amount to convert");
     let from = fromCurrency.value;
     let to = toCurrency.value;
-    let result = (amt * currencyTypes[from][to]).toFixed(2);
+    let result = 0;
+    (from == to) ? result = amt : result = (amt * currencyTypes[from][to]).toFixed(2);
+
     // console.log(result);
+
     let notification = showResult(to, result)
-    document.getElementById("amountResult").innerHTML = `${parseInt(amt)} ${from}=${notification} ${to}`;
+    document.getElementById("amountResult").innerHTML = `${amt} ${from}=${notification} ${to}`;
 })
 
-function convertCurrency() {
-    let amount = document.getElementById('amount');
-    let from = document.getElementById('currency1');
-    let to = document.getElementById('currency2');
-    let result = 0;
-
-    result = (amount * currencyTypes[from][to]).toFixed(2);
-}
-
-function formatCurrency(type1, type2, value) {
-    const formatter = new Intl.NumberFormat(type1, {
-        currency: type2,
+function formatCurrency(type, value) {
+    const formatter = new Intl.NumberFormat(type, {
+        currency: type,
         style: "currency"
     });
     return formatter.format(value);
@@ -75,11 +68,11 @@ function formatCurrency(type1, type2, value) {
 
 const showResult = (to, result) => {
     let convertedResult = "";
-    to === "USD" ? convertedValue = formatCurrency('en-US', "USD", result) : ""
-    to === "EUR" ? convertedValue = formatCurrency('it-IT', "EUR", result) : ""
-    to === "VND" ? convertedValue = formatCurrency('vi', "VND", result) : ""
-    to === "KRW" ? convertedValue = formatCurrency('kr-KR', "KRW", result) : ""
-    to === "IDR" ? convertedValue = formatCurrency('id-ID', "IDR", result) : ""
+    to === "USD" ? convertedResult = formatCurrency('USD', result) : ""
+    to === "EUR" ? convertedResult = formatCurrency('EUR', result) : ""
+    to === "VND" ? convertedResult = formatCurrency('VND', result) : ""
+    to === "KRW" ? convertedResult = formatCurrency('KRW', result) : ""
+    to === "IDR" ? convertedResult = formatCurrency('IDR', result) : ""
     return convertedResult;
 }
 
